a42608a1ff808ef706f1dc27f19591497cfbf137
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2023-11-09 17:25:21 +0100

    actulizado ejercicio 3

--- unidad-04/unidad-04-ejercicios/T4_Ejercicio3/app/src/main/java/t4/ejercicio3/MainActivity.java
+++ unidad-04/unidad-04-ejercicios/T4_Ejercicio3/app/src/main/java/t4/ejercicio3/MainActivity.java
@@ -47,6 +47,12 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
     public void streetView(View view) {
+        Intent intent = new
+        Intent(Intent.ACTION_VIEW,Uri.parse("google.streetview:cbll=38.996766, -0.1652696&cbp=0,250,0,0,0")); //cbll=latitud,longitud&cbp=0,azimut,0,zoom,altura
+        startActivity(intent);
+
+        // A continuación está mi otro intento de visualizar street view
+        /*
         double latitud = 36.95038964026492;
         double longitud = -4.546720595064295;
         String uri = "google.streetview:cbll=" + latitud + "," + longitud;
@@ -54,5 +60,6 @@ public class MainActivity extends AppCompatActivity {
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
         mapIntent.setPackage("com.google.android.apps.maps");  // Esto asegura que se abra en la aplicación de Google Maps
         startActivity(mapIntent);
+         */
     }
 }
