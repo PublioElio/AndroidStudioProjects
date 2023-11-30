5079e1aa837bfff619d1d99799f6be691544d19a
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2023-11-30 13:03:45 +0100

    practica final

---
+++ unidad-05/practica-evaluacion/app/src/main/java/com/example/practica_evaluacion/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.practica_evaluacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button bntAcceder = findViewById(R.id.btnAcceder);
+
+        bntAcceder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PrincipalActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
