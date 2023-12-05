a8f4dd6d61086e8483e4b3f4b576cfb6bac42979
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2023-12-05 13:06:04 +0100

    apuntes de checkbox en Android

--- unidad-05/unidad-05-apuntes-09/app/src/main/java/com/example/unidad_05_apuntes_09/MainActivity.java
+++ unidad-05/unidad-05-apuntes-09/app/src/main/java/com/example/unidad_05_apuntes_09/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.unidad_05_apuntes_09;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Recupero el id del listado
+        final ListView listado = findViewById(R.id.listado);
+
+        // Defino los datos
+        ArrayList<Datos> datos = new ArrayList<Datos>();
+        datos.add(new Datos("Texto 1", false));
+        datos.add(new Datos("Texto 2", false));
+        datos.add(new Datos("Texto 3", false));
+        datos.add(new Datos("Texto 4", false));
+
+        // Crear adaptador
+        Adaptador adaptador = new Adaptador(this, datos);
+        listado.setAdapter(adaptador);
+
+
     }
 }
