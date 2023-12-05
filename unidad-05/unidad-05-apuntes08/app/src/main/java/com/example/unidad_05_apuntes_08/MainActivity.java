dd3814cb4bc752e977e512629815446124be0b34
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2023-12-05 12:37:44 +0100

    apuntes finalizados

--- unidad-05/unidad-05-apuntes08/app/src/main/java/com/example/unidad_05_apuntes_08/MainActivity.java
+++ unidad-05/unidad-05-apuntes08/app/src/main/java/com/example/unidad_05_apuntes_08/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.unidad_05_apuntes_08;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -24,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
         // menú info nos da la posición del elemento
         AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
 
+        // la línea siguiente sirve para que haya un header en el menú contextual
+        menu.setHeaderTitle(listado.getAdapter().getItem(info.position).toString().toUpperCase());
+
         switch (info.position) {
             case 0:
                 inflater.inflate(R.menu.menu_c1, menu);
@@ -32,6 +38,28 @@ public class MainActivity extends AppCompatActivity {
                 inflater.inflate(R.menu.menu_c2, menu);
                 break;
         }
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+
+        String texto;
+        int id = item.getItemId();
+
+        // Realizo una accion partiendo del elemento del menú que he pulsado
+        if (id == R.id.mc1Op1 || id == R.id.mc1Op2 || id == R.id.mc1Op3 || id == R.id.mc2Op1) {
+            texto = item.getTitle().toString();
+            Toast.makeText(this, texto, Toast.LENGTH_SHORT).show();
+        }
+
+        // Obtengo información del elemento sobre el que hemos desplegado el menú
+        AdapterView.AdapterContextMenuInfo info =
+                (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        int pulsado = info.position;
+        texto = listado.getItemAtPosition(pulsado).toString();
+        Toast.makeText(this, texto, Toast.LENGTH_SHORT).show();
+
+        return super.onContextItemSelected(item);
     }
 
     @Override
