903b10558016a89e45e2eb6a226d53cf742f0522
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-01-09 12:28:29 +0100

    desarrollo ejemplo

--- unidad-07/apuntes/unidad-07-apuntes-00/app/src/main/java/com/example/unidad_05_apuntes_00/MainActivity.java
+++ unidad-07/apuntes/unidad-07-apuntes-00/app/src/main/java/com/example/unidad_05_apuntes_00/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.unidad_05_apuntes_00;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        final Button guardar = findViewById(R.id.btnGuardar);
+        final Button cargar = findViewById(R.id.btnCargar);
+
+        guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Obtengo la referencia de la colección de preferencias (archivo XML)
+                // donde tengo o voy a guardar las preferencias
+                SharedPreferences prefs =
+                        getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
+
+            }
+        });
+
+
+
     }
 }
