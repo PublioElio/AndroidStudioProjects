02d8dc19ef4393146f8a7d83b19a28a97c50ccbf
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-01-23 14:13:21 +0100

    ejercicio05

--- unidad-07/ejercicios/ejercicio-05/app/src/main/java/com/example/ejercicio_05/MainActivity.java
+++ unidad-07/ejercicios/ejercicio-05/app/src/main/java/com/example/ejercicio_05/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.ejercicio_05;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.os.Environment.DIRECTORY_RINGTONES;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +28,51 @@ public class MainActivity extends AppCompatActivity {
         final Button recuperar = findViewById(R.id.btnRecuperar);
         final EditText myEditText = findViewById(R.id.myEditTxt);
         final TextView myTextView = findViewById(R.id.myTextView);
+
+        // Hay que comprobar el estado de la memoria externa (SD)
+        boolean sdDisponible = false;
+        boolean sdAccesoEscritura = false;
+        String estado = Environment.getExternalStorageState();
+        if (estado.equals(Environment.MEDIA_MOUNTED)) {
+            sdDisponible = true;
+            sdAccesoEscritura = true;
+        } else if (estado.equals(Environment.MEDIA_MOUNTED_READ_ONLY)) {
+            sdDisponible = true;
+        }
+
+        if (sdDisponible && sdAccesoEscritura) {
+
+            guardar.setOnClickListener(view -> {
+                try {
+                    File ruta_sd = getExternalFilesDir(null);
+                    File f = new File(ruta_sd.getAbsolutePath(), "prueba_sd.txt");
+                    OutputStreamWriter fout = new OutputStreamWriter(new FileOutputStream(f));
+                    fout.write(myEditText.getText().toString());
+                    fout.close();
+                } catch (Exception ex) {
+                    Log.e("Ficheros", "Error al escribir en la tarjeta SD");
+                }
+            });
+        } else {
+            Log.e("Ficheros", "La tarjeta SD no está disponible para acceso o escritura");
+        }
+
+        if (sdDisponible) {
+            recuperar.setOnClickListener(view -> {
+                try {
+                    File ruta_sd = Environment.getExternalStorageDirectory();
+                    assert ruta_sd != null;
+                    File f = new File(ruta_sd.getAbsolutePath(), "prueba_sd.txt");
+                    BufferedReader fin =
+                            new BufferedReader(new InputStreamReader(new FileInputStream(f)));
+                    myTextView.setText(fin.readLine());
+                    fin.close();
+                } catch (Exception ex) {
+                    Log.e("Ficheros", "Error al leer en la tarjeta SD");
+                }
+            });
+        } else {
+            Log.e("Ficheros", "Tarjeta de memoria SD no disponible");
+        }
     }
 }
