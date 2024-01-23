85461d701fb450f6cf41503b18d644b7a37d34dc
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-01-23 12:39:15 +0100

    ejercicio 4 realizado

--- unidad-07/ejercicios/ejercicio-03/app/src/main/java/com/example/ejercicio_03/MainActivity.java
+++ unidad-07/ejercicios/ejercicio-03/app/src/main/java/com/example/ejercicio_03/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.ejercicio_03;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.SwitchPreference;
 
@@ -14,17 +13,6 @@ public class MainActivity extends PreferenceActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.opciones);
-
-        final SwitchPreference activationTime =
-                (SwitchPreference) findPreference("activationTime");
-
-        activationTime.setOnPreferenceChangeListener((preference, newValue) -> {
-            boolean activationTimeEnabled = (Boolean) newValue;
-            findPreference("start").setEnabled(activationTimeEnabled);
-            findPreference("end").setEnabled(activationTimeEnabled);
-            findPreference("repit").setEnabled(activationTimeEnabled);
-            return true;
-        });
 
         findPreference("additionalSettings").setOnPreferenceClickListener(preference -> {
             Intent intent = new Intent(MainActivity.this, OpcionesActivity.class);
---
+++ unidad-07/ejercicios/ejercicios-04/app/src/main/java/com/example/ejercicio_04/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.ejercicio_04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Inicializo las variables
+        final Button guardar = findViewById(R.id.btnGuardar);
+        final Button recuperar = findViewById(R.id.btnRecuperar);
+        final EditText myEditText = findViewById(R.id.myEditTxt);
+        final TextView myTextView = findViewById(R.id.myTextView);
+
+        // Guardo el texto del Edit Text en el archivo de memoria interna
+        guardar.setOnClickListener(view -> {
+            try {
+                OutputStreamWriter miFichero =
+                        new OutputStreamWriter(openFileOutput("fichero.txt", Context.MODE_PRIVATE));
+                miFichero.write(myEditText.getText().toString());
+                myTextView.setText("");
+                miFichero.close();
+            } catch (Exception ex) {
+                Log.e("Ficheros", "Error al escribir fichero en memoria interna");
+            }
+        });
+
+        // Cargo el texto del Edit Text en el archivo de memoria interna
+        recuperar.setOnClickListener(view -> {
+            try {
+                BufferedReader miFichero =
+                        new BufferedReader(new InputStreamReader(openFileInput("fichero.txt")));
+                myTextView.setText(miFichero.readLine());
+                miFichero.close();
+            } catch (Exception e) {
+                Log.e("Ficheros","Error al leer desde la memoria interna");
+            }
+        });
+
+
+    }
+}
