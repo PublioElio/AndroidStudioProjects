05bcb72b4fac0d5557fe3c9f946aa34b3262988e
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-04-28 20:49:26 +0200

    new changes

--- proyecto_integrado/proyecto/app/src/main/java/com/example/saved_by_the_call/NewCallActivity.java
+++ proyecto_integrado/proyecto/app/src/main/java/com/example/saved_by_the_call/NewCallActivity.java
@@ -1,18 +1,16 @@
 package com.example.saved_by_the_call;
 
-import android.content.Intent;
 import android.os.Bundle;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class NewCallActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, NewContactActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_new_call);
     }
 }
--- proyecto_integrado/proyecto/app/src/main/java/com/example/saved_by_the_call/NewContactFormActivity.java
+++
@@ -1,4 +0,0 @@
-package com.example.saved_by_the_call;
-
-public class NewContactFormActivity {
-}
