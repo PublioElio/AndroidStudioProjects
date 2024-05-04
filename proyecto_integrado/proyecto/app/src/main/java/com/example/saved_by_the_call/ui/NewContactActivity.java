871e8fceb49af89de2429ddeb1ef2a873cca8eb6
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-05-04 17:13:36 +0200

    image picker

--- proyecto_integrado/proyecto/app/src/main/java/com/example/saved_by_the_call/ui/NewContactActivity.java
+++ proyecto_integrado/proyecto/app/src/main/java/com/example/saved_by_the_call/ui/NewContactActivity.java
@@ -1,18 +1,74 @@
 package com.example.saved_by_the_call.ui;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.saved_by_the_call.R;
 
 public class NewContactActivity extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_READ_EXTERNAL_STORAGE = 1;
+    ActivityResultLauncher<Intent> resultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_new_contact);
+
+        final TextView addContactImg = findViewById(R.id.edTxtContactImg);
+        registerResult();
+
+        addContactImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(NewContactActivity.this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    pickImg();
+
+                } else {
+
+                    ActivityCompat.requestPermissions(NewContactActivity.this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            PERMISSION_REQUEST_READ_EXTERNAL_STORAGE);
+                }
+            }
+        });
+
+
+
+
+    }
+
+    private void pickImg() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        resultLauncher.launch(intent);
+    }
+
+    private void registerResult() {
+        resultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    try {
+                        Uri imageUri = result.getData().getData();
+                    } catch (Exception exception) {
+                        Toast.makeText(NewContactActivity.this,
+                                "No ha seleccionado ninguna imagen",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
