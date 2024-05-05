acc6b31fabfe27a43859d016e52107f46617593d
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-05-05 18:28:54 +0200

    changes on menu

--- proyecto_integrado/proyecto/app/src/main/java/com/example/saved_by_the_call/ui/NewCallActivity.java
+++ proyecto_integrado/proyecto/app/src/main/java/com/example/saved_by_the_call/ui/NewCallActivity.java
@@ -28,7 +28,7 @@ public class NewCallActivity extends AppCompatActivity {
         final EditText contactName = findViewById(R.id.edTxtCallContactName);
         final EditText datePickerNewCall = findViewById(R.id.datePickerNewCall);
         final EditText timePickerNewCall = findViewById(R.id.timePickerNewCall);
-        final Button addCallButton = findViewById(R.id.btnAddContact);
+        final Button btnAddNewCall = findViewById(R.id.btnAddNewCall);
 
         // launches DatePicker on a Dialog
         datePickerNewCall.setOnClickListener(view -> showDatePickerDialog(datePickerNewCall));
@@ -36,13 +36,16 @@ public class NewCallActivity extends AppCompatActivity {
         // launches TimePicker on a Dialog
         timePickerNewCall.setOnClickListener(view -> showTimePickerDialog(timePickerNewCall));
 
-        addCallButton.setOnClickListener(view -> {
+        // Checks all fields and creates a new call in the database
+        btnAddNewCall.setOnClickListener(view -> {
             final TextView txtViewCallFormInfo = findViewById(R.id.txtViewCallFormInfo);
             if (checkFields(callName, contactName, datePickerNewCall, timePickerNewCall)) {
+                // TODO: CREAR LA LLAMADA EN LA BBDD
                 Toast.makeText(getApplicationContext(), R.string.call_created_confirmation_msg,
                         Toast.LENGTH_SHORT).show();
                 txtViewCallFormInfo.setTextColor(ContextCompat.getColor(view.getContext(),
                         R.color.black));
+                resetFields(callName, contactName, datePickerNewCall, timePickerNewCall);
             } else
                 txtViewCallFormInfo.setTextColor(ContextCompat.getColor(view.getContext(),
                         R.color.red));
@@ -50,10 +53,27 @@ public class NewCallActivity extends AppCompatActivity {
     }
 
     /**
+     * This method resets all fields to their default values.
+     *
+     * @param callName          call name EditText field
+     * @param contactName       contact name EditText field
+     * @param datePickerNewCall date EditText field
+     * @param timePickerNewCall time EditText field
+     */
+    private void resetFields(EditText callName, EditText contactName, EditText datePickerNewCall,
+                             EditText timePickerNewCall) {
+        callName.setText(R.string.name);
+        contactName.setText(R.string.name);
+        datePickerNewCall.setText(R.string.choose_date_hint);
+        timePickerNewCall.setText(R.string.choose_time_hint);
+    }
+
+    /**
      * This method checks that all fields are correctly filled out, displaying/hiding error messages
      * in the corresponding fields.
-     * @param callName call name EditText field
-     * @param contactName contact name EditText field
+     *
+     * @param callName          call name EditText field
+     * @param contactName       contact name EditText field
      * @param datePickerNewCall date EditText field
      * @param timePickerNewCall time EditText field
      * @return true if all fields are correct
@@ -61,10 +81,11 @@ public class NewCallActivity extends AppCompatActivity {
     private boolean checkFields(EditText callName, EditText contactName, EditText datePickerNewCall,
                                 EditText timePickerNewCall) {
 
-        TextView fieldCallNameWarning = findViewById(R.id.edTxCallNameWarning);
-        TextView fieldCallContactNameWarning = findViewById(R.id.edTxCallContactNameWarning);
-        TextView fieldCallDateWarning = findViewById(R.id.edTxCallDateWarning);
-        TextView fieldCallTimeWarning = findViewById(R.id.edTxCallTimeWarning);
+        // Warnings for each field
+        final TextView fieldCallNameWarning = findViewById(R.id.edTxtCallNameWarning);
+        final TextView fieldCallContactNameWarning = findViewById(R.id.edTxtCallContactNameWarning);
+        final TextView fieldCallDateWarning = findViewById(R.id.edTxtCallDateWarning);
+        final TextView fieldCallTimeWarning = findViewById(R.id.edTxtCallTimeWarning);
         boolean allFieldsCorrect = true;
 
         if (!checkFieldName(callName)) {
@@ -91,50 +112,104 @@ public class NewCallActivity extends AppCompatActivity {
         if (!checkFieldTime(timePickerNewCall)) {
             fieldCallTimeWarning.setVisibility(View.VISIBLE);
             allFieldsCorrect = false;
-        } else{
+        } else {
             fieldCallTimeWarning.setVisibility(View.INVISIBLE);
         }
 
         return allFieldsCorrect;
     }
 
+    /**
+     * This method checks that the call name is at least 3 characters long and not empty.
+     *
+     * @param callName call name EditText field
+     * @return true if the field is correct
+     */
     private boolean checkFieldName(EditText callName) {
-        return String.valueOf(callName.getText()).length() >= 3;
+        return !callName.getText().toString().isEmpty() &&
+                String.valueOf(callName.getText()).length() >= 3;
     }
 
     // TODO: MODIFICAR ESTE MÉTODO PARA QUE COMPRUEBE LA BBDD BUSCANDO EL CONTACTO
     private boolean checkFieldContact(EditText contactName) {
-        return String.valueOf(contactName.getText()).length() >= 3;
+        boolean correctContact = !contactName.getText().toString().isEmpty();
+        if (correctContact) {
+    // TODO: AQUÍ SE DEBERÍA COMPROBAR QUE EL CONTACTO EXISTE EN LA BBDD
+        }
+        return correctContact;
     }
 
+    /**
+     * This method checks that the date is in the format dd/mm/yyyy.
+     *
+     * @param datePickerNewCall date EditText field
+     * @return true if the field is correct
+     */
     private boolean checkFieldDate(EditText datePickerNewCall) {
         return String.valueOf(datePickerNewCall.getText()).matches("\\d{2}/\\d{2}/\\d{4}");
     }
 
+    /**
+     * This method checks that the time is in the format hh:mm.
+     *
+     * @param timePickerNewCall time EditText field
+     * @return true if the field is correct
+     */
     private boolean checkFieldTime(EditText timePickerNewCall) {
         return String.valueOf(timePickerNewCall.getText()).matches("\\d{2}:\\d{2}");
     }
 
+    /**
+     * This method shows a TimePickerDialog.
+     *
+     * @param timePickerNewCall time EditText field
+     */
     private void showTimePickerDialog(EditText timePickerNewCall) {
         TimePickerFragment newFragment = TimePickerFragment.newInstance((view, hour, min) ->
                 timePickerNewCall.setText(formatTime(hour, min)));
         newFragment.show(getSupportFragmentManager(), "timePicker");
     }
 
+    /**
+     * This method shows a DatePickerDialog.
+     *
+     * @param datePickerNewCall date EditText field
+     */
     private void showDatePickerDialog(EditText datePickerNewCall) {
         DatePickerFragment newFragment = DatePickerFragment.newInstance((view, year, month, day) ->
                 datePickerNewCall.setText(formatDate(day, month + 1, year)));
         newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 
+    /**
+     * This method formats a number to a two-digit number.
+     *
+     * @param num number to format
+     * @return formatted number
+     */
     private String formatNumber(int num) {
         return (num <= 9) ? "0" + num : String.valueOf(num);
     }
 
+    /**
+     * This method formats a date to the format dd/mm/yyyy.
+     *
+     * @param day   day
+     * @param month month
+     * @param year  year
+     * @return formatted date
+     */
     private String formatDate(int day, int month, int year) {
         return formatNumber(day) + "/" + formatNumber(month) + "/" + year;
     }
 
+    /**
+     * This method formats a time to the format hh:mm.
+     *
+     * @param hour hour
+     * @param min  minute
+     * @return formatted time
+     */
     private String formatTime(int hour, int min) {
         return formatNumber(hour) + ":" + formatNumber(min);
     }
--- proyecto_integrado/proyecto/app/src/main/java/com/example/saved_by_the_call/ui/NewContactActivity.java
+++ proyecto_integrado/proyecto/app/src/main/java/com/example/saved_by_the_call/ui/NewContactActivity.java
@@ -4,6 +4,9 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,16 +22,7 @@ import com.example.saved_by_the_call.R;
 
 public class NewContactActivity extends AppCompatActivity {
     private static final int PERMISSION_REQUEST_READ_EXTERNAL_STORAGE = 1;
-    ActivityResultLauncher<PickVisualMediaRequest> pickMedia =
-            registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
-                if (uri != null) {
-                    Log.d("PhotoPicker", "Selected URI: " + uri);
-                } else {
-                    Toast.makeText(NewContactActivity.this,
-                            R.string.no_img_selected,
-                            Toast.LENGTH_SHORT).show();
-                }
-            });
+    ActivityResultLauncher<PickVisualMediaRequest> pickMedia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,25 +30,143 @@ public class NewContactActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_new_contact);
 
+        // Get the fields
+        final EditText contactName = findViewById(R.id.edTxtContactName);
+        final EditText contactPhone = findViewById(R.id.edTxtContactPhone);
         final TextView addContactImg = findViewById(R.id.edTxtContactImg);
+        final Button btnAddContact = findViewById(R.id.btnAddNewContact);
 
+        // Image picker
+        pickMedia = registerForActivityResult(new ActivityResultContracts.PickVisualMedia(),
+                uri -> {
+                    if (uri != null) {
+                        Log.d("PhotoPicker", "Selected URI: " + uri);
+                        addContactImg.setText(R.string.contact_added_img_hint);
+                    } else {
+                        Toast.makeText(NewContactActivity.this,
+                                R.string.no_img_selected,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        // Add contact image
         addContactImg.setOnClickListener(view -> {
             if (ContextCompat.checkSelfPermission(NewContactActivity.this,
                     Manifest.permission.READ_EXTERNAL_STORAGE)
                     == PackageManager.PERMISSION_GRANTED) {
                 pickImg();
             } else {
-
                 ActivityCompat.requestPermissions(NewContactActivity.this,
                         new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
                         PERMISSION_REQUEST_READ_EXTERNAL_STORAGE);
             }
         });
+
+        // Add contact button
+        btnAddContact.setOnClickListener(view -> {
+            final TextView txtViewContactFormInfo = findViewById(R.id.txtViewContactFormInfo);
+            if (checkFields(contactName, contactPhone)) {
+                // TODO: CREAR LA CONTACTO EN LA BBDD
+                Toast.makeText(getApplicationContext(),
+                        R.string.contact_created_confirmation_msg, Toast.LENGTH_SHORT).show();
+                txtViewContactFormInfo.setTextColor(ContextCompat.getColor(view.getContext(),
+                        R.color.black));
+                txtViewContactFormInfo.setText(R.string.asterisk);
+                resetFields(contactName, contactPhone, addContactImg);
+            } else {
+                txtViewContactFormInfo.setTextColor(ContextCompat.getColor(view.getContext(),
+                        R.color.red));
+                txtViewContactFormInfo.setText(R.string.new_contact_incorrect_fields_warning);
+            }
+        });
+
     }
 
+    /**
+     * This method resets the fields to their default values.
+     *
+     * @param contactName  contact name EditText field
+     * @param contactPhone contact phone EditText field
+     * @param addContactImg contact image TextView field
+     */
+    private void resetFields(EditText contactName, EditText contactPhone, TextView addContactImg) {
+        contactName.setText(R.string.name);
+        contactPhone.setText(R.string.phone);
+        addContactImg.setText(R.string.upload_img_hint);
+    }
+
+    /**
+     * This method launches the image picker.
+     */
     private void pickImg() {
         pickMedia.launch(new PickVisualMediaRequest.Builder()
                 .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
                 .build());
     }
+
+
+    /**
+     * This method checks that all fields are correctly filled out, displaying/hiding error messages
+     * in the corresponding fields.
+     *
+     * @param contactName  contact name EditText field
+     * @param contactPhone contact phone EditText field
+     * @return true if all fields are correct
+     */
+    private boolean checkFields(EditText contactName, EditText contactPhone) {
+
+        // Warnings for each field
+        final TextView edTxtContactNameWarning = findViewById(R.id.edTxtNewContactNameWarning);
+        final TextView edTxtContactPhoneWarning = findViewById(R.id.edTxtNewContactPhoneWarning);
+
+        boolean allFieldsCorrect = true;
+
+        if (!checkFieldName(contactName)) {
+            edTxtContactNameWarning.setVisibility(View.VISIBLE);
+            allFieldsCorrect = false;
+        } else {
+            edTxtContactNameWarning.setVisibility(View.INVISIBLE);
+        }
+
+        if (!checkFieldPhone(contactPhone)) {
+            edTxtContactPhoneWarning.setVisibility(View.VISIBLE);
+            allFieldsCorrect = false;
+        } else {
+            edTxtContactPhoneWarning.setVisibility(View.INVISIBLE);
+        }
+        return allFieldsCorrect;
+    }
+
+
+    private boolean checkFieldName(EditText contactName) {
+        boolean correctName = !contactName.getText().toString().isEmpty() &&
+                contactName.getText().toString().length() >= 3;
+        if (correctName) {
+            // TODO: AQUÍ SE DEBERÍA COMPROBAR QUE EL CONTACTO EXISTE EN LA BBDD
+        }
+        return correctName;
+    }
+
+    /**
+     * This method checks that the phone number is at least 3 characters long, not empty and
+     * contains only digits.
+     *
+     * @param contactPhone phone number EditText field
+     * @return true if the field is correct
+     */
+    private boolean checkFieldPhone(EditText contactPhone) {
+        String phone = contactPhone.getText().toString();
+        return !phone.isEmpty() && phone.length() >= 3 && allAreNumbers(phone);
+    }
+
+    /**
+     * This method checks if all the characters in a string are numbers.
+     *
+     * @param s string to check
+     * @return true if all characters are numbers
+     */
+    public static boolean allAreNumbers(String s) {
+        return s.chars().allMatch(Character::isDigit);
+    }
+
 }
