c942f199c5a6d2181c109a9b8a51b1117d2b1507
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-06-03 22:17:06 +0200

    added loading screen

---
+++ proyecto_integrado/proyecto/app/src/main/java/com/example/saved_by_the_call/ui/LoadingScreenActivity.java
@@ -0,0 +1,34 @@
+package com.example.saved_by_the_call.ui;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.example.saved_by_the_call.R;
+
+public class LoadingScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_loading_screen);
+
+        ConstraintLayout layout = findViewById(R.id.loadingScreenLayout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) layout.getBackground();
+        animationDrawable.start();
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(LoadingScreenActivity.this, MainActivity.class);
+            startActivity(intent);
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            finish();
+        }, 3000);
+
+    }
+}
