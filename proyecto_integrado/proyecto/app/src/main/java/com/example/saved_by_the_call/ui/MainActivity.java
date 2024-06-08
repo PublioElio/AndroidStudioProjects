c232c0915087149814830dcc5d61f30ed12cbcd4
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-06-08 17:39:34 +0200

    main view done

--- proyecto_integrado/proyecto/app/src/main/java/com/example/saved_by_the_call/ui/MainActivity.java
+++ proyecto_integrado/proyecto/app/src/main/java/com/example/saved_by_the_call/ui/MainActivity.java
@@ -1,14 +1,13 @@
 package com.example.saved_by_the_call.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.view.ContextThemeWrapper;
+import androidx.appcompat.widget.PopupMenu;
 
 import com.example.saved_by_the_call.R;
 import com.example.saved_by_the_call.ui.top_menu.TopMenu;
@@ -21,32 +20,60 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        final Toolbar toolbar = findViewById(R.id.toolbar_top_menu);
-        setSupportActionBar(toolbar);
+        final ImageView contacts = findViewById(R.id.imgViewContactsMain);
+        final ImageView calls = findViewById(R.id.imgViewCallsMain);
+
+        setupPopupMenu(contacts,
+                R.menu.pop_menu_contacts_main,
+                R.id.pop_menu_contacts_main_option_1,
+                NewContactActivity.class,
+                R.id.pop_menu_contacts_main_option_2,
+                ContactListActivity.class);
+        setupPopupMenu(calls,
+                R.menu.pop_menu_calls_main,
+                R.id.pop_menu_calls_main_option_1,
+                NewCallActivity.class,
+                R.id.pop_menu_calls_main_option_2,
+                NewCallActivity.class);
     }
 
     /**
-     * This method creates the top menu.
+     * Setup the popup menu for the image view.
      *
-     * @param menu menu
-     * @return true if the menu is created
+     * @param imageView    The image view.
+     * @param menuResId    The menu resource id.
+     * @param option1Id    The option 1 id.
+     * @param option1Class The option 1 class.
+     * @param option2Id    The option 2 id.
+     * @param option2Class The option 2 class.
      */
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        TopMenu.inflateMenu(inflater, menu, R.menu.top_menu);
-        return true;
-    }
+    private void setupPopupMenu(ImageView imageView, int menuResId, int option1Id,
+                                Class<?> option1Class, int option2Id, Class<?> option2Class) {
+        imageView.setOnClickListener(v -> {
+            PopupMenu popupMenu = new PopupMenu(new ContextThemeWrapper(MainActivity.this,
+                    R.style.PopupMenuMain), imageView);
+            popupMenu.getMenuInflater().inflate(menuResId, popupMenu.getMenu());
 
-    /**
-     * This method handles the top menu options.
-     *
-     * @param item selected item
-     * @return true if the option is handled
-     */
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        return TopMenu.onOptionsItemSelected(this, item) ||
-                super.onOptionsItemSelected(item);
+            popupMenu.setOnMenuItemClickListener(item -> {
+                int id = item.getItemId();
+                boolean result = TopMenu.onOptionsItemSelected(MainActivity.this, item);
+                Intent intent = null;
+                if (id == option1Id) {
+                    intent = new Intent(MainActivity.this, option1Class);
+                    result = true;
+                } else if (id == option2Id) {
+                    intent = new Intent(MainActivity.this, option2Class);
+                    result = true;
+                }
+                if (intent != null) {
+                    MainActivity.this.startActivity(intent);
+                }
+                return result;
+            });
+
+            popupMenu.show();
+        });
     }
 }
+
+
